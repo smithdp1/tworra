eeebf5cb0c4a7b93b7d3caad9baec98811b0e11d
David Smith <=>
2025-05-06 11:27:07 -0700

    Fix for DB string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       // add scheema to allow longer strings in mysql
+        Schema::defaultStringLength(191);
+        
     }
 }
